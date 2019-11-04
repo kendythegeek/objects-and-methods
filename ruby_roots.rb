8da14e88f9802f857424022fbde0886d5078ce32
kendythegeek <chantellekyendereta@gmail.com>
2019-11-04 18:07:41 +0300

    Expounded on methods, paramters and arguments

---
+++ ruby_roots.rb
@@ -0,0 +1,22 @@
+def my_name
+    "Chantelle"
+end
+puts my_name
+
+def age_difference(age_of_mother, age_of_daughter)
+    age_of_mother - age_of_daughter
+end
+puts age_difference(56, 19)
+
+def have_valid_id(valid_id = "no")
+    "Mother has #{valid_id} valid id."
+end
+puts have_valid_id()
+
+def total_deposit(*deposit)
+    "deposited ksh#{deposit.sum} in my bank account. "
+end
+puts total_deposit(1500, 500, 5400, 6578, 5490)
+
+#output deposited ksh[1500, 500, 5400, 6578, 5490] in my bank account. 
+    
